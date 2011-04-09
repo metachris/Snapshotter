0411328060afeb3c3aed69c18b217f38d3d61581
Chris Hager <chager@myyearbook.com>
2011-04-08 20:42:16 -0400

    refactoring, added original url

--- snapshot.py
+++ snapshot.py
@@ -5,50 +5,50 @@ from os import system
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 
+ext = "png"
+ext_thumb = "gif"
 
-def snapshot(url_pivot, url_orig, fn, ext):
-    print "Create screenshot of original"
-    system("/opt/wkhtmltoimage-amd64 --crop-h 1024 %s '/tmp/%s-orig.%s'" % (url_orig, fn, ext))
-    print "- Create thumbnail"
-    system("convert -resize 128x128 '/tmp/%s-orig.%s' '/tmp/%s-orig_thumb.%s'" % (fn, ext, fn, ext))
-    print "- Minify screenshot"
-    system("mogrify -resize 1024x1024 '/tmp/%s-orig.%s'" % (fn, ext))
 
-    print "Create screenshot of pivot"
-    system("/opt/wkhtmltoimage-amd64 --crop-h 1024 %s '/tmp/%s.%s'" % (url_pivot, fn, ext))
+def snapshot(url, fn, s3bucket=None):
+    print "Capture %s" % url
+    system("/opt/wkhtmltoimage-amd64 --crop-h 1024 %s '/tmp/%s.%s'" % (url, fn, ext))
+
     print "- Create thumbnail"
-    system("convert -resize 128x128 '/tmp/%s.%s' '/tmp/%s_thumb.%s'" % (fn, ext, fn, ext))
-    print "- Minify screenshot"
+    system("convert -resize 156x156 '/tmp/%s.%s' '/tmp/%s_thumb.%s'" % (fn, ext, fn, ext_thumb))
+
+    print "- Shrink original"
     system("mogrify -resize 1024x1024 '/tmp/%s.%s'" % (fn, ext))
 
-    print "Copy into s3"
+    if not s3bucket:
+        return
+
+    print "- Move into s3 bucket"
+
+    k = Key(s3bucket)
+    k.key = "%s.%s" % (fn, ext)
+    k.set_contents_from_filename("/tmp/%s.%s" % (fn, ext))
+    k.set_acl('public-read')
+
+    k = Key(s3bucket)
+    k.key = "%s_thumb.%s" % (fn, ext_thumb)
+    k.set_contents_from_filename("/tmp/%s_thumb.%s" % (fn, ext_thumb))
+    k.set_acl('public-read')
+
+    system("rm /tmp/%s.%s" % (fn, ext))
+    system("rm /tmp/%s_thumb.%s" % (fn, ext_thumb))
+
+
+def snapshot_pivot(url_pivot, url_orig, fn):
     conn = S3Connection()
     bucket = conn.create_bucket('csspivot_snapshots')
 
-    k = Key(bucket)
-    k.key = "%s.%s" % (fn, ext)
-    k.set_contents_from_filename("/tmp/%s.%s" % (fn, ext))
+    snapshot(url_pivot, fn, bucket)
+    snapshot(url_orig, "%s-orig" % fn, bucket)
 
-    k = Key(bucket)
-    k.key = "%s_thumb.%s" % (fn, ext)
-    k.set_contents_from_filename("/tmp/%s_thumb.%s" % (fn, ext))
-
-    k = Key(bucket)
-    k.key = "%s-orig.%s" % (fn, ext)
-    k.set_contents_from_filename("/tmp/%s-orig.%s" % (fn, ext))
-
-    k = Key(bucket)
-    k.key = "%s-orig_thumb.%s" % (fn, ext)
-    k.set_contents_from_filename("/tmp/%s-orig_thumb.%s" % (fn, ext))
-
-    system("rm /tmp/%s.%s" % (fn, ext))
-    system("rm /tmp/%s_thumb.%s" % (fn, ext))
-    system("rm /tmp/%s-orig.%s" % (fn, ext))
-    system("rm /tmp/%s-orig_thumb.%s" % (fn, ext))
 
 if __name__ == "__main__":
-    if len(argv) < 5:
-        print "Use: $0 [url_pivot] [url_orig] [image-fnbase] [image-ext]"
+    if len(argv) < 4:
+        print "Use: $0 [url_pivot] [url_orig] [image-fnbase]"
         exit(1)
 
-    snapshot(argv[1], argv[2], argv[3], argv[4])
+    snapshot_pivot(argv[1], argv[2], argv[3])
--- snapshotter.py
+++ snapshotter.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import web
-from snapshot import snapshot
+import snapshot
 
 urls = (
-    "/.*", "hello",
     "/snap/(.*)", "snap",
+    "/.*", "hello",
 )
 
 app = web.application(urls, globals())
@@ -19,11 +19,14 @@ class hello:
 
 class snap:
     def GET(self, pivot_id):
-        if pivot_id:
-            snapshot("http://www.csspivot.com/%s" % pivot_id, "http://www.csspivot.com/%s" % pivot_id, pivot_id, "png")
-        return 'Hello, world!'
+        i = web.input()
+        url = i.url
+        if pivot_id and url:
+            snapshot.snapshot_pivot("http://www.csspivot.com/proxy/%s" % \
+                    pivot_id, url, pivot_id)
+            return "https://s3.amazonaws.com/csspivot_snapshots/%s"
 
 
 if __name__ == "__main__":
-    web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
+    #web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
     app.run()
